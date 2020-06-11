5da713e88ce9611216691817e1f2b86fbe23968d
TLBN <tete.leticia@gmail.com>
2020-06-11 14:57:08 -0300

    Initial commit

---
+++ src/test/java/com/minhabiblioteca/MinhaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhabiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
