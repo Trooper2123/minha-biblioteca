c0f5b0dc25c49bbcf71e7b2da9862aca9be5d425
TLBN <tete.leticia@gmail.com>
2020-06-12 17:31:16 -0300

    esqueleto de um CRUD

--- src/main/java/com/minhabiblioteca/MinhaBibliotecaApplication.java
+++ src/main/java/com/minhabiblioteca/MinhaBibliotecaApplication.java
@@ -1,13 +1,27 @@
 package com.minhabiblioteca;
 
+import model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import repository.BooksRepository;
+
+import java.util.List;
 
 @SpringBootApplication
-public class MinhaBibliotecaApplication {
+public class MinhaBibliotecaApplication implements CommandLineRunner {
+	@Autowired(required=true)
+	private BooksRepository booksRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MinhaBibliotecaApplication.class, args);
+	@Override
+	public void run(String... args) throws Exception {
+		List<Book> books = booksRepository.findAll();
+		System.out.println("Rodou");
 	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(MinhaBibliotecaApplication.class, args);
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,28 @@
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS subjects;
+
+CREATE TABLE subject(
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  theme VARCHAR(250) NOT NULL
+);
 
 CREATE TABLE books (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
-  subject VARCHAR(250) NOT NULL,
-  n_pages INT DEFAULT NULL
+  subject Int DEFAULT NULL,
+  n_pages INT DEFAULT NULL,
+  FOREIGN KEY (subject) REFERENCES subject (id)
 );
 
-INSERT INTO BOOKS (name, subject, n_pages) VALUES
-  ('O Aparicionista', 'Ghotic', 200);
+INSERT INTO subject (theme) VALUES
+  ('Classico'),
+  ('Terror'),
+  ('Romance');
+
+INSERT INTO BOOKS (name,subject, n_pages) VALUES
+  ('O Aparicionista',1, 150),
+  ('Cujo',2, 400),
+  ('O Iluminado',2,  350),
+  ('A Princesa da Lapa',3,  200),
+  ('Dracula',1, 500);
+
